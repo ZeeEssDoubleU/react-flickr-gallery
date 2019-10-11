4b97c5396216f065af2689ad52f7658d5c94bef1
Zak Williams <zak.williams2287@gmail.com>
2019-10-11 12:43:50 -0400

    fresh install of CRA

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# React Flickr Gallery
 
+Project built with React that pulls data from Flickr API.
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv/config");
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
