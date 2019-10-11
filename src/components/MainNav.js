a0bf752c7a47b041228488ee8e4fb41f85e6259e
Zak Williams <zak.williams2287@gmail.com>
2019-10-11 06:08:34 -0400

    fixed env vars

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 // Navigation component below the search componenet on the app page.  Displays 3 NavLink buttons.
 const MainNav = props => {
-  return (
-    <nav className="main-nav">
-      <ul>
-        <li><NavLink to='/cats'>Cats</NavLink></li>
-        <li><NavLink to='/dogs'>Dogs</NavLink></li>
-        <li><NavLink to='/falafels'>Falafels</NavLink></li>
-      </ul>
-    </nav>
-  );
-}
+	return (
+		<nav className="main-nav">
+			<ul>
+				<li>
+					<NavLink to="/cats">Cats</NavLink>
+				</li>
+				<li>
+					<NavLink to="/dogs">Dogs</NavLink>
+				</li>
+				<li>
+					<NavLink to="/falafels">Falafels</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
+};
 
 export default MainNav;
